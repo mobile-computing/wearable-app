da94e2c81befcdabd42ebebd717513331790e916
Frederick Zhang <frederick888@tsundere.moe>
2017-10-03 22:23:54 +1100

    basic anims

--- app/src/main/java/frederick/tsundere/evafast/ActionActivity.java
+++ app/src/main/java/frederick/tsundere/evafast/ActionActivity.java
@@ -2,17 +2,29 @@ package frederick.tsundere.evafast;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
+import android.support.wearable.view.BoxInsetLayout;
+import android.widget.Toast;
+
+import frederick.tsundere.evafast.listener.OnSwipeTouchListener;
 
 public class ActionActivity extends WearableActivity {
-    private TextView mTextView;
+    private BoxInsetLayout mActionContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        mActionContainer = (BoxInsetLayout) findViewById(R.id.container_action);
+
+        mActionContainer.setOnTouchListener(new OnSwipeTouchListener(ActionActivity.this) {
+            @Override
+            public boolean onSwipeDown() {
+                Toast.makeText(ActionActivity.this, "Triggered", Toast.LENGTH_SHORT).show();
+                finish();
+                return true;
+            }
+        });
 
         // Enables Always-on
         setAmbientEnabled();
--- app/src/main/java/frederick/tsundere/evafast/MainActivity.java
+++ app/src/main/java/frederick/tsundere/evafast/MainActivity.java
@@ -19,25 +19,25 @@ public class MainActivity extends WearableActivity {
 
         mMainContainer = (BoxInsetLayout) findViewById(R.id.container_main);
         mImageViewArrow = (ImageView) findViewById(R.id.imageview_arrow);
+        mImageViewArrow.setImageDrawable(getDrawable(R.drawable.ic_arrow_noun_816167));
 
         // Enables Always-on
         setAmbientEnabled();
 
         mMainContainer.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
-            private void startActionActivity() {
-                Intent actionActivity = new Intent(MainActivity.this, ActionActivity.class);
-                startActivity(actionActivity);
-            }
-
             @Override
             public boolean onSwipeLeft() {
-                startActionActivity();
+                Intent actionActivity = new Intent(MainActivity.this, ActionActivity.class);
+                startActivity(actionActivity);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
                 return true;
             }
 
             @Override
             public boolean onSwipeUp() {
-                startActionActivity();
+                Intent actionActivity = new Intent(MainActivity.this, ActionActivity.class);
+                startActivity(actionActivity);
+                overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up);
                 return true;
             }
         });
